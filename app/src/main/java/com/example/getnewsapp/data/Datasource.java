af29018624707b0682ee6fe6184d5bd0ca4dbd54
Tnluser <tnluser@ADMINs-MacBook-Pro.local>
2022-09-09 08:44:48 +0530

    UI front

---
+++ app/src/main/java/com/example/getnewsapp/data/Datasource.java
@@ -0,0 +1,2 @@
+package com.example.getnewsapp.data;public class Datasource {
+}
---
+++ app/src/main/java/com/example/getnewsapp/model/News.kt
@@ -0,0 +1,7 @@
+package com.example.getnewsapp.model
+
+data class News (val s:Int){
+    fun loadNews():List<News>{
+
+    }
+}
